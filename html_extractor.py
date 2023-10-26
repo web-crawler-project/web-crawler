a31be27ebfbcf2a6588b64cdd93f5c47c1b49400
Caden <63772723+cadencjk@users.noreply.github.com>
2023-10-27 02:21:17 +0800

    Create html_extractor.py

---
+++ html_extractor.py
@@ -0,0 +1,40 @@
+import json
+from bs4 import BeautifulSoup
+
+# Function to extract data (headers, paragraphs) and links from HTML
+# @param html_content: HTML content of the page
+# @return: JSON data and links
+def extract_html(html_content):
+    # Create a BeautifulSoup object from html content
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    extracted_data = {}
+
+    # Extract headers
+    headers = []
+    for header in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
+        if header.get_text():
+            headers.append(header.get_text().strip())
+
+    if headers:
+        extracted_data['headers'] = headers
+
+    # Extract paragraph data
+    paragraphs = []
+    for paragraph in soup.find_all('p'):
+        if paragraph.get_text():
+            paragraphs.append(paragraph.get_text().strip())
+
+    if paragraphs:
+        extracted_data['paragraphs'] = paragraphs
+
+    # Extract links
+    links = []
+    for link in soup.find_all('a', href=True):
+        if link['href'].startswith('http') or link['href'].startswith('https'):
+            links.append(link['href'])
+
+    # Convert data to JSON
+    json_data = json.dumps(extracted_data)
+
+    return json_data, links
