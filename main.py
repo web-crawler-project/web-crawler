39bcb528c34ea3b672e42f0a41f9ec94fccef780
venuslimm <limjiayivenus@gmail.com>
2023-10-28 19:03:26 +0800

    Update the way program gets and processes json data and urls

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
 import json
 import queue
 
-# Read urls from input file, returns an array of urls
-# Only reads unique urls (assuming there can be duplicate urls in the file)
-def read_urls():
-  urls = queue.Queue()
-  seen_urls = set()
-  
+# Returns a set of unique urls that are already in data.json
+def read_processed_urls():
+  json_data = {}
+  try:
+    with open(output_file, 'r') as file:
+      try:
+        # Read existing JSON data
+        json_data = json.load(file)
+      except json.JSONDecodeError:
+        pass
+  except IOError as e:
+    print(f'Error reading {output_file}: {e}')
+  return set(json_data.keys())
+
+# Returns a set of unique urls that are already in urls.txt
+def read_new_urls():
   try:
     # TODO: Use file access lock mechanism
     with open(input_file, 'r') as file:
-      for line in file:
-        url = line.strip()
-        if url and url not in seen_urls:
-          urls.put(url)
-          seen_urls.add(url)
+      urls = {line.strip() for line in file if line.strip()}
   except IOError as e:
     print(f'Error reading {input_file}: {e}')
-    
+    urls = set()
   return urls
 
-# Returns unique urls in data that hasnt been crawled
-# Compare with urls in urls.txt file
-# Instead of keeping track of the url in program for multithreading purposes
-# Instead of data.json file, assuming data.json and urls.txt are in sync
-def remove_duplicate_urls(data):
-  current_urls = read_urls()
-  unique_data = [d for d in data if not any(url in current_urls.queue for url in d) and d]
-  return unique_data
+# Returns a queue of unique urls that are in urls.txt not in data.json (i.e. unprocessed urls)
+def read_unprocessed_urls():
+  processed_urls = read_processed_urls()
+  new_urls = read_new_urls()
+  unique_urls = new_urls - processed_urls
+  unique_queue = queue.Queue()
+  for url in unique_urls:
+    unique_queue.put(url)
+  print(f'Unique unprocessed urls: {list(unique_queue.queue)}')
+  return unique_queue
 
-# Writes unique urls to urls.txt
-def write_urls(data):
-  urls = [key for d in data for key in d.keys()]
+def remove_trailing_slash_from_urls(new_urls):
+  return [url.rstrip('/') for url in new_urls]
+
+def remove_duplicate_urls(new_urls):
+  return set(new_urls)
+
+# Returns unique urls that are not in urls.txt and data.json
+def remove_existing_urls(urls):
+  processed_urls = read_processed_urls()
+  new_urls = read_new_urls()
+  return urls - processed_urls - new_urls
+
+# Writes unique unprocessed urls to urls.txt
+def write_urls(urls):
   print(f'Writing the following urls to {input_file}: {urls}')
-  
   try:
     # TODO: Use file access lock mechanism
     with open(input_file, 'a') as file:
@@ -42,9 +60,8 @@ def write_urls(data):
   except IOError as e:
     print(f'Error writing {output_file}: {e}')
 
-# Only write unique url and data to the output file
-# Assuming data originated from the url that just got scraped
-def write_data(new_data, origin_url):
+# Write all the urls and extracted data to data.json
+def write_data(new_data):
   try:
     # TODO: Use file access lock mechanism
     with open(output_file, 'r+') as file:
@@ -55,12 +72,9 @@ def write_data(new_data, origin_url):
         # If file is empty, initialize with empty JSON data
         json_data = {}
       
-      # Append new URLs to JSON data
-      print(f'Writing the following urls to {output_file}:')
-      for one_new_data in new_data:
-        print(one_new_data)
-        for url, xml in one_new_data.items():
-          json_data[url] = {'xml': xml, 'origin': origin_url}
+      # Append new data to existing/empty JSON data
+      print(f'Writing the new data to {output_file}: {new_data}')
+      json_data.update(new_data)
       
       # Write updated JSON data to file
       file.seek(0)
@@ -74,24 +88,25 @@ if __name__ == '__main__':
   input_file = './urls.txt'
   output_file = './data.json'
   
-  # urls - queue variable
-  urls = read_urls()
+  urls = read_unprocessed_urls()
   
-  # Process each url
   # TODO: Incorporate multiprocessing here
   while not urls.empty():
     url = urls.get()
     print(f'Processing {url}...')
     
-    # TODO: Crawl with the url and get back an array of {'new url': 'xml data'}
-    # New url to strip trailing '/'
-    # Assuming data only has unique links
-    data = [{'www.google.com': 'xml'}, {'www.apple.com': 'xml'},
-            {'www.test.com': 'xml'}, {'www.orange.com': 'xml'}]
+    # TODO: Crawl with the url and get back HTML content
+    # TODO: Extract HTML content
+    json_data, new_urls = {}, ['www.googles.com', 'www.apples.com']
     
-    # unique_data - array of {'new url': 'xml data'}
-    unique_data = remove_duplicate_urls(data)
-    write_urls(unique_data)
-    write_data(unique_data, url)
+    # Process new urls
+    new_urls = remove_trailing_slash_from_urls(new_urls)
+    # Assuming urls returned are not unique
+    new_urls = remove_duplicate_urls(new_urls)
+    # Remove urls that already exist in urls.txt
+    new_unique_urls = remove_existing_urls(new_urls)
     
-    # TODO: Add criteria to end the program (break from loop) if necessary
+    # Write to files
+    write_urls(new_unique_urls)
+    write_data({url: {'html': json_data, 'urls': list(new_urls)}})
+    
