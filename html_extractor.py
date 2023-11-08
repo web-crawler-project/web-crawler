8ca5fa0ac9e5a67ac757e2ff6512a348b555f83c
Caden <63772723+cadencjk@users.noreply.github.com>
2023-11-09 02:05:15 +0800

    Update html_extractor.py

--- html_extractor.py
+++ html_extractor.py
@@ -9,13 +9,14 @@ import re
 def extract_html(soup, keywords):
     extracted_data = {}
 
-    # Extract headers
+    # Extract HTML headers from h1 to h6
     headers = []
     for header in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
         if header.get_text():
             headers.append(header.get_text().strip())
 
     if headers and keywords:
+        # Check if headers do not contain any keyword specified
         if not contain_keyword(headers, keywords):
             return None, None
 
@@ -30,21 +31,23 @@ def extract_html(soup, keywords):
     if paragraphs:
         extracted_data['paragraphs'] = paragraphs
 
-    # Extract links
+    # Extract hyperlinks from page
     links = []
     for link in soup.find_all('a', href=True):
-        # Filter only http or https urls
+        # Filter only http or https urls with no extensions
         if link['href'].startswith('http') and check_html_link(link['href']):
             links.append(link['href'])
 
+    # Return the data extracted (headers and paragraphs), with the links found
     return extracted_data, links
 
-
+# Function to check if any of the headers contain a keyword specified
 def contain_keyword(headers, keywords):
     headers_split = []
     for header in headers:
         raw_split_header = header.lower().split()
         for word in raw_split_header:
+            # Remove all non-alphanumerical characters
             headers_split.append(re.sub(r'\W+', '', word))
 
     for header in headers_split:
@@ -52,7 +55,7 @@ def contain_keyword(headers, keywords):
             return True
     return False
 
-
+# Function to check if parsed url does not contain any extension
 def check_html_link(url):
     parsed_url = urlparse(url)
     path = parsed_url.path
